4cf0584b138dfaf5b98e9b365b9bf91cbec67b23
Alice Miller <1181355+weakmint@users.noreply.github.com>
2025-06-26 11:44:34 -0400

    fix docker build

--- nix-docker-builds/flake.nix
+++ nix-docker-builds/flake.nix
@@ -13,83 +13,45 @@
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 lastModifiedDate;
 
-      # System types to support.
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      # Thanks nix-darwin!
+      system = "aarch64-linux";
 
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (
-        system:
-        import nixpkgs {
-          system = "x86_64-linux";
-        }
-      );
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in
     {
 
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          bin = pkgs.buildGoModule {
-            pname = "hello-server";
-            inherit version;
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
-            src = ./.;
+      bin = pkgs.buildGoModule {
+        pname = "hello-server";
+        inherit version;
+        # In 'nix develop', we don't need a copy of the source tree
+        # in the Nix store.
+        src = ./.;
 
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake hash and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remember to bump this hash when your dependencies change.
-            # vendorHash = pkgs.lib.fakeHash;
+        # This hash locks the dependencies of this package. It is
+        # necessary because of how Go requires network access to resolve
+        # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+        # details. Normally one can build with a fake hash and rely on native Go
+        # mechanisms to tell you what the hash should be or determine what
+        # it should be "out-of-band" with other tooling (eg. gomod2nix).
+        # To begin with it is recommended to set this, but one must
+        # remember to bump this hash when your dependencies change.
+        # vendorHash = pkgs.lib.fakeHash;
 
-            vendorHash = null;
-          };
+        vendorHash = null;
+      };
 
-          docker = pkgs.dockerTools.buildLayeredImage {
-            name = "hello-server";
-            tag = "latest";
-            config.Cmd = "${self.packages.${system}.bin}/bin/helloserver";
-          };
-        }
-      );
-
-      # Add dependencies that are only needed for development
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              go
-              gopls
-              gotools
-              go-tools
-            ];
-          };
-        }
-      );
+      docker = pkgs.dockerTools.buildLayeredImage {
+        name = "hello-server";
+        tag = "latest";
+        config.Cmd = "${self.bin}/bin/helloserver";
+      };
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
       # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.bin);
+      defaultPackage = self.bin;
     };
 }
